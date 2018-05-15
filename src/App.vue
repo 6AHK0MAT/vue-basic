18888d5db346cd44d1ca12f1583f57c7d097727a
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-15 15:26:54 +0700

    Блок 4. Теория. Компоненты Связь дочерних компонентов

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,29 @@
   <div>
     <h1>Parent: {{ carName }}</h1>
 
+    <app-counter :counter="counter"></app-counter>
+
     <app-car
       :carName="carName"
       :carYear="carYear"
+      :counter="counter"
       :changeFunc="changeNameToAudi"
       @nameChanged="carName = $event"
+      @counterUpdated="counter = $event"
     ></app-car>
   </div>
 </template>
 
 <script>
 import Car from './Car.vue'
+import Counter from './Counter.vue'
 
 export default {
   data () {
     return {
       carName: 'Ford',
-      carYear: 2018
+      carYear: 2018,
+      counter: 0
     }
   },
   methods: {
@@ -27,7 +33,8 @@ export default {
     }
   },
   components: {
-    appCar: Car
+    appCar: Car,
+    appCounter: Counter
   }
 }
 </script>
