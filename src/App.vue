6020c7f2627825cb0bc2d34d2b4c2fc95f4bf2a5
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-15 17:08:21 +0700

    Блок 5. Теория. Директивы Локальная регистрация

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h2 v-colored:background.font="'red'">{{ title }}</h2>
-    <h2 v-colored:color.font.delay="'blue'">{{ title }}</h2>
+    <h2 v-colored:color.delay.font="'blue'">{{ title }}</h2>
+
+    <h2 v-font>Local font directive</h2>
   </div>
 </template>
 
@@ -11,6 +13,13 @@ export default {
   data () {
     return {
       title: 'Hello I am Vue!'
+    }
+  },
+  directives: {
+    font: {
+      bind(el, bindings, vnode) {
+        el.style.fontSize = '80px'
+      }
     }
   }
 }
